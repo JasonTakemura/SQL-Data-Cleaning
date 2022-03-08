ead7f7890640d65a98fc744cd711d590c2a4c982
JasonTakemura <91088835+JasonTakemura@users.noreply.github.com>
2022-03-08 15:37:37 -0800

    Add files via upload

---
+++ SQLDataCleaning.sql
@@ -0,0 +1,112 @@
+--Data Cleaning Nashville Housing Data
+--Skills Used: Joins, CTEs, Case Statements, Converting Data Types, Substrings, Altering Tables
+
+--Remove the unused time portion of "SaleDate"
+
+SELECT SaleDate, CONVERT(Date, SaleDate)
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+	ALTER COLUMN SaleDate DATE
+
+--Use property information to fill NULL values
+
+SELECT *
+FROM NashvilleHousing
+WHERE PropertyAddress IS NULL
+
+SELECT A.ParcelID, A.PropertyAddress, B.ParcelID, B.PropertyAddress, ISNULL(A.PropertyAddress, B.PropertyAddress)
+FROM NashvilleHousing A
+JOIN NashvilleHousing B
+	ON A.ParcelID = B.ParcelID
+	AND A.[UniqueID ] <> B.[UniqueID ]
+WHERE A.PropertyAddress IS NULL
+
+UPDATE A
+SET PropertyAddress = ISNULL(A.PropertyAddress, B.PropertyAddress)
+FROM NashvilleHousing A
+JOIN NashvilleHousing B
+	ON A.ParcelID = B.ParcelID
+	AND A.[UniqueID ] <> B.[UniqueID ]
+WHERE A.PropertyAddress IS NULL
+
+-- Separate full address columns into different columns for street address, city, and state
+
+SELECT SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) - 1) AS Address,
+	   SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 2, LEN(PropertyAddress)) AS Address
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD PropertyAddressStreet NVARCHAR(255)
+
+UPDATE NashvilleHousing
+SET PropertyAddressStreet = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) - 1)
+
+ALTER TABLE NashvilleHousing
+ADD PropertyAddressCity NVARCHAR(255)
+
+UPDATE NashvilleHousing
+SET PropertyAddressCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) + 2, LEN(PropertyAddress))
+
+SELECT PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3), 
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2), 
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+FROM NashvilleHousing
+
+ALTER TABLE NashvilleHousing
+ADD OwnerAddressStreet NVARCHAR(255)
+
+UPDATE NashvilleHousing
+SET OwnerAddressStreet = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerAddressCity NVARCHAR(255)
+
+UPDATE NashvilleHousing
+SET OwnerAddressCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+
+ALTER TABLE NashvilleHousing
+ADD OwnerAddressState NVARCHAR(255)
+
+UPDATE NashvilleHousing
+SET OwnerAddressState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+-- Make entries in "SoldAsVacant" field consistent - change Y to Yes and N to No
+
+SELECT DISTINCT(SoldAsVacant), COUNT(SoldAsVacant)
+FROM NashvilleHousing
+GROUP BY SoldAsVacant
+ORDER BY 2
+
+SELECT SoldAsVacant,
+CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	 WHEN SoldAsVacant = 'N' THEN 'No'
+	 ELSE SoldAsVacant
+	 END
+FROM NashvilleHousing
+
+UPDATE NashvilleHousing
+SET SoldAsVacant = 
+CASE WHEN SoldAsVacant = 'Y' THEN 'Yes'
+	 WHEN SoldAsVacant = 'N' THEN 'No'
+	 ELSE SoldAsVacant
+	 END
+
+-- Remove duplicate rows
+
+
+WITH RowNum AS(
+SELECT *,
+	ROW_NUMBER() OVER (
+	PARTITION BY ParcelID,
+				 PropertyAddress,
+				 SalePrice,
+				 SaleDate,
+				 LegalReference
+				 ORDER BY UniqueID
+				 ) row_num
+FROM NashvilleHousing
+)
+DELETE 
+FROM RowNum
+WHERE row_num > 1
